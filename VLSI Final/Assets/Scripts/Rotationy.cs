e911f82f3d0d6fe3c0ab9860229aadae22155840
Yash Gupta <yashprakash28@users.noreply.github.com>
2020-10-29 00:41:08 +0530

    Added AR-VLSI
    Roation used to demonstrate VLSI fabrication process.

---
+++ VLSI Final/Assets/Scripts/Rotationy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotationy : MonoBehaviour
+{
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3 (0,0,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement);
+    }
+}
---
+++ VLSI Final/Assets/Scripts/VLSIScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VLSIScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject ingot, lattice, wafer, film, uv, pcb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour[] vrb = GetComponentsInChildren<VirtualButtonBehaviour> ();
+        for(int i=0 ; i<6 ; i++)
+        {
+            vrb[i].RegisterEventHandler(this);
+        }
+
+        ingot.SetActive(false);
+        lattice.SetActive(false);
+        wafer.SetActive(false);
+        film.SetActive(false);
+        uv.SetActive(false);
+        pcb.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if(vb.VirtualButtonName == "LatticeVB")
+        {
+            Debug.Log("OpAmpButton Pressed");
+            ingot.SetActive(false);
+            lattice.SetActive(true);
+            wafer.SetActive(false);
+            film.SetActive(false);
+            uv.SetActive(false);
+            pcb.SetActive(false);
+        }
+        else if(vb.VirtualButtonName == "WaferVB")
+        {
+            Debug.Log("LeadVersionButton Pressed");
+        ingot.SetActive(false);
+        lattice.SetActive(false);
+        wafer.SetActive(true);
+        film.SetActive(false);
+        uv.SetActive(false);
+        pcb.SetActive(false);          
+        }
+        else if(vb.VirtualButtonName == "FilmVB")
+        {
+            Debug.Log("LeadVersionButton Pressed");
+        ingot.SetActive(false);
+        lattice.SetActive(false);
+        wafer.SetActive(false);
+        film.SetActive(true);
+        uv.SetActive(false);
+        pcb.SetActive(false);          
+        }
+        else if(vb.VirtualButtonName == "UVVB")
+        {
+            Debug.Log("LeadVersionButton Pressed");
+        ingot.SetActive(false);
+        lattice.SetActive(false);
+        wafer.SetActive(false);
+        film.SetActive(false);
+        uv.SetActive(true);
+        pcb.SetActive(false);        
+        }
+        else if(vb.VirtualButtonName == "PCBVB")
+        {
+            Debug.Log("LeadVersionButton Pressed");
+        ingot.SetActive(false);
+        lattice.SetActive(false);
+        wafer.SetActive(false);
+        film.SetActive(false);
+        uv.SetActive(false);
+        pcb.SetActive(true);
+        }
+        else if(vb.VirtualButtonName == "IngotVB")
+        {
+            Debug.Log("LeadVersionButton Pressed");
+        ingot.SetActive(true);
+        lattice.SetActive(false);
+        wafer.SetActive(false);
+        film.SetActive(false);
+        uv.SetActive(false);
+        pcb.SetActive(false);          
+        }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName + "Virtual Button Not Supported");
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Virtual Button Pressed");
+    }
+}
